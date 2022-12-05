7c8243d2b4bec36582c0fed18225b837c487ed93
Polaczeq22 <mikulamichal@pm.me>
2022-12-05 10:52:57 +0100

    Poprawki w działaniu RegExp, overflow: scroll

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
 const checkRegexp = function(input){
     console.log(input);
-    const pattern = new RegExp("^[0-9]*[.][0-9]+$","gm");
+    const pattern = new RegExp("^([0-9]+)(\.?)([0-9]{0,2})$","gm");
     if(pattern.test(input)){
         return true 
     } else return false;
